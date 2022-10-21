ee7d3cb3ac65d3e24a618bf7d2e87784992c55b8
floshen18 <florenceshen18@gmail.com>
2022-10-21 21:05:27 +0000

    Add method to get tokens and ner tags with seq_len=512

--- common.py
+++ common.py
@@ -18,26 +18,32 @@ label_list = [
     ]
 id2label = {i: label for i, label in enumerate(label_list)}
 label2id = {label: i for i, label in enumerate(label_list)}
+seq_len = 512
     
 def get_tokens_and_ner_tags(filename):
     with open(filename, 'r', encoding="utf-8") as f:
         lines = f.readlines()
         tokens, entities = [], []
+        token_counter = 0
         current_tokens, current_entities = [], []
         for line in lines:
             data = line.split(" ")
-            if len(data) < 2:
-                if not current_tokens:
-                    continue
-                tokens.append(current_tokens)
-                entities.append(current_entities)
-                current_tokens, current_entities = [], []
-            elif len(data) == 2:
+            if len(data) != 2:
+                continue
+            if token_counter < seq_len:
                 current_tokens.append(data[0])
                 current_entities.append(data[1].strip())
+                token_counter += 1
+            else:
+                tokens.append(current_tokens)
+                entities.append(current_entities)
+                token_counter = 0
+                current_tokens, current_entities = [], []
         if current_tokens:
             tokens.append(current_tokens)
             entities.append(current_entities)
+            
+    df = pd.DataFrame({'tokens': tokens, 'ner_tags': entities})
     return pd.DataFrame({'tokens': tokens, 'ner_tags': entities})
   
 def get_dataset(directory):
@@ -70,45 +76,24 @@ class CustomCallback(TrainerCallback):
             self._trainer.evaluate(eval_dataset=self._trainer.train_dataset, metric_key_prefix="train")
             return control_copy
 
-'''
-class DataSequence(torch.utils.data.Dataset):
-
-    def __init__(self, df, tokenizer, max_len, labels_to_ids, label_all_tokens):
-        lb = [i.split() for i in df['labels'].values.tolist()]
-        txt = df['text'].values.tolist()
-        self.texts, self.labels = [], []
-        for i, j in zip(txt, lb):
-            tokenized_i = tokenizer(i, padding='max_length', max_length = max_len, truncation=True, return_tensors="pt")
-            self.texts.append(tokenized_i)
-            word_ids = tokenized_i.word_ids()
-            previous_word_idx = None
-            label_ids = []
-            for word_idx in word_ids:
-                if word_idx is None:
-                    label_ids.append(-100)
-                elif word_idx != previous_word_idx:
-                    label_ids.append(labels_to_ids.get(j[word_idx], -100))
-                else:
-                    label_ids.append(labels_to_ids.get(j[word_idx], -100) if label_all_tokens else -100)
-                previous_word_idx = word_idx
-            self.labels.append(label_ids)
-
-    def __len__(self):
-
-        return len(self.labels)
-
-    def get_batch_data(self, idx):
-
-        return self.texts[idx]
-
-    def get_batch_labels(self, idx):
-
-        return torch.LongTensor(self.labels[idx])
-
-    def __getitem__(self, idx):
-
-        batch_data = self.get_batch_data(idx)
-        batch_labels = self.get_batch_labels(idx)
-
-        return batch_data, batch_labels
-'''
+def get_tokens_and_ner_tags_by_sentence(filename):
+    with open(filename, 'r', encoding="utf-8") as f:
+        lines = f.readlines()
+        tokens, entities = [], []
+        current_tokens, current_entities = [], []
+        for line in lines:
+            data = line.split(" ")
+            if len(data) < 2:
+                if not current_tokens:
+                    continue
+                tokens.append(current_tokens)
+                entities.append(current_entities)
+                current_tokens, current_entities = [], []
+            elif len(data) == 2:
+                current_tokens.append(data[0])
+                current_entities.append(data[1].strip())
+        if current_tokens:
+            tokens.append(current_tokens)
+            entities.append(current_entities)
+    df = pd.DataFrame({'tokens': tokens, 'ner_tags': entities})
+    return pd.DataFrame({'tokens': tokens, 'ner_tags': entities})
