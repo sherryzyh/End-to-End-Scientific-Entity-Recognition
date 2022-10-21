bb8be577d0e0751f4c85d4bc9da11ee2df6d65f4
sherryzyh <sherryyh.z@gmail.com>
2022-10-21 12:31:18 -0400

    fix bugs in collector

--- raw_data_collecter.py
+++ raw_data_collecter.py
@@ -14,9 +14,13 @@ class ACLScraper:
     def __init__(self, workingdir = '/content/drive/MyDrive/NLP'):
         os.chdir(workingdir)
         self.cnt = 0
+        self.summary = os.path.join(workingdir, "summary.txt")
+        with open(self.summary, "w", encoding="utf-8") as f:
+            f.write("Scrapper Summary\n\n")
 
     def prepareACLInfoForYear(self, year):
         if year > 2022 or year < 2000:
+            self.printsummary(year, "acl")
             print(f"{year} is a Wrong Year for ACL, Please Try Another Year")
             return
         self.page_url = f"https://aclanthology.org/events/acl-{year}"
@@ -32,6 +36,7 @@ class ACLScraper:
 
     def prepareEMNLPInfoForYear(self, year):
         if year >= 2022 or year < 2010:
+            self.printsummary(year, "emnlp")
             print(f"{year} is a Wrong Year for EMNLP, Please Try Another Year")
             return False
         self.page_url = f"https://aclanthology.org/events/emnlp-{year}/"
@@ -46,6 +51,7 @@ class ACLScraper:
     def prepareNAACLInfoForYear(self, year):
         invalidYears = {2011, 2014, 2017, 2020}
         if year > 2022 or year < 2010 or year in invalidYears:
+            self.printsummary(year, "naacl")
             print(f"{year} is a Wrong Year for NAACL, Please Try Another Year")
             return False
         self.page_url = f"https://aclanthology.org/events/naacl-{year}/"
@@ -59,17 +65,29 @@ class ACLScraper:
 
     def getACLsForYear(self, year, num_limit = None):
         if self.prepareACLInfoForYear(year):
-            self.scrape(year, num_limit)
+            self.scrape(year, "acl", num_limit)
 
     def getNAACLsForYear(self, year, num_limit = None):
         if self.prepareNAACLInfoForYear(year):
-            self.scrape(year, num_limit)
+            self.scrape(year, "naacl", num_limit)
 
     def getEMNLPsForYear(self, year, num_limit = None):
         if self.prepareEMNLPInfoForYear(year):
-            self.scrape(year, num_limit)
+            self.scrape(year, "emnlp", num_limit)
 
-    def scrape(self, year, num_limit):
+    def getEachConferenceForYear(self, year, num_limit = None):
+        self.getACLsForYear(year, num_limit)
+        self.getNAACLsForYear(year, num_limit)
+        self.getEMNLPsForYear(year, num_limit)
+
+    def printsummary(self, year, conference, num=None):
+        with open(self.summary, "a", encoding="utf-8") as f:
+            if num is None:
+                f.write(f'{conference:6}\t{year:4}\t{"N/A":6} paper\n')
+            else:
+                f.write(f'{conference:6}\t{year:4}\t{num:6} paper\n')
+
+    def scrape(self, year, conference, num_limit):
         # %%
         html_doc = requests.get(self.page_url).text
         soup = BeautifulSoup(html_doc, 'html.parser')
@@ -83,6 +101,8 @@ class ACLScraper:
             paper_a = paper_span.strong.a
             title = paper_a.get_text()
             url = "https://aclanthology.org" + paper_a['href']
+            if "Proceedings" in title:
+                continue
             paper_list.append([title, url, pdf_url])
             if num_limit is not None:
                 if len(paper_list) >= num_limit:
@@ -90,16 +110,16 @@ class ACLScraper:
         with open(self.conf_name + '.json', 'w', encoding='utf8') as f:
             json.dump(paper_list, f, indent = 2, ensure_ascii= False)
 
-        print(f'Scraped total {len(paper_list)} papers for {self.conf_name}')
+        self.printsummary(year, conference, len(paper_list))
 
         if not os.path.exists(self.conf_name):
             os.mkdir(self.conf_name)
 
         illegal_chr = r'\/:*?<>|'
         table = ''.maketrans('', '', illegal_chr)
-        for i, paper in tqdm(list(enumerate(paper_list))):
+        for i, paper in list(enumerate(paper_list)):
             r = requests.get(paper[2])
-            n = '{}.{}.pdf'.format( i +1, paper[0].translate(table))
+            n = '{}.{}.{}.pdf'.format(year, conference, paper[0].translate(table))
             with open('./{}/{}'.format(self.conf_name, n), 'wb') as f:
                 f.write(r.content)
             self.cnt += 1
@@ -113,12 +133,12 @@ class RawDataCollector:
                  tokenized_root="tokenized_paper",
                  annotate_data_root="annotation_paper"):
         self.pdf_root = os.path.join(raw_data_root, pdf_root)
-        # print(f"pdf_root: {self.pdf_root}")
         self.parsed_root = os.path.join(raw_data_root, parsed_root)
-        # print(f"parsed_root: {self.parsed_root}")
         self.tokenized_root = os.path.join(raw_data_root, tokenized_root)
-        # print(f"tokenized_root: {self.tokenized_root}")
         self.annotate_data_root = os.path.join(raw_data_root, annotate_data_root)
+
+        if not os.path.exists(self.pdf_root):
+            os.mkdir(self.pdf_root)
         if not os.path.exists(self.parsed_root):
             os.mkdir(self.parsed_root)
         if not os.path.exists(self.tokenized_root):
@@ -130,29 +150,30 @@ class RawDataCollector:
         self.tokenizer = MyTokenizer()
 
     def collect_pdf_papers(self):
-        for year in range(2015, 2023):
-            if 2015 <= year <= 2020:
-                self.scraper.getACLsForYear(year, 5)
-                self.scraper.getNAACLsForYear(year, 5)
-                self.scraper.getEMNLPsForYear(year, 5)
-            else:
-                self.scraper.getACLsForYear(year, 10)
-                self.scraper.getNAACLsForYear(year, 10)
-                self.scraper.getEMNLPsForYear(year, 10)
+        for year in range(2014, 2015):
+            if year < 2015:
+                self.scraper.getEachConferenceForYear(year, 1)
+            elif 2015 <= year <= 2020:
+                self.scraper.getEachConferenceForYear(year, 5)
+                # self.scraper.getACLsForYear(year, 5)
+                # self.scraper.getNAACLsForYear(year, 5)
+                # self.scraper.getEMNLPsForYear(year, 5)
+            elif year > 2020:
+                self.scraper.getEachConferenceForYear(year, 10)
+                # self.scraper.getACLsForYear(year, 10)
+                # self.scraper.getNAACLsForYear(year, 10)
+                # self.scraper.getEMNLPsForYear(year, 10)
 
     def parse_papers(self, source, destination):
         nlp_parser = spacy.load('en_core_web_sm')
         for folder in os.listdir(source):
             if not os.path.isdir(os.path.join(source, folder)):
                 continue
-            folder_info = folder.split("_")
-            conference, year = folder_info[0], folder_info[1]
+
             for pdf in os.listdir(os.path.join(source, folder)):
                 reader = PdfReader(os.path.join(source, folder, pdf))
                 article_info = pdf.split(".")
-                order, article = article_info[0], article_info[1]
-                if order == "1":
-                    continue
+                year, conference, article, _  = article_info
                 txt = year + "_" + conference + "_" + article + ".txt"
                 save_as = os.path.join(destination, txt)
                 with open(save_as, "w", encoding="utf-8") as txt_file:
@@ -208,5 +229,5 @@ if __name__=="__main__":
     project_root = os.getcwd()
     DataCollector = RawDataCollector(raw_data_root=os.path.join(project_root, "Raw_Data"))
     DataCollector.prep_raw_data(tokenize=True,
-                                parse=False,
-                                collect=False)
+                                parse=True,
+                                collect=True)
--- utils.py
+++ utils.py
@@ -6,6 +6,17 @@ for maintainability, wheel reusability and code consistency :>
 import spacy
 from spacy.lang.en import English
 from spacy.tokenizer import Tokenizer
+import os
+# import openai
+# import json
+# from openapi_schema_to_json_schema import to_json_schema
+# from dotenv import load_dotenv
+# from ratelimit import limits, RateLimitException, sleep_and_retry
+# consts for rate limiting feature
+MAX_CALLS_PER_MINUTE = 20
+ONE_MINUTE = 60
+
+
 
 class MyTokenizer:
     def __init__(self):
@@ -27,51 +38,46 @@ class MyTokenizer:
         token_space_str = "".join([x.text + " " for x in tokens])
         token_label_str = "".join([x.text + " O\n" for x in tokens])
         return token_space_str, token_label_str
-import os
-import openai
-import json
-from openapi_schema_to_json_schema import to_json_schema
-from dotenv import load_dotenv
-from ratelimit import limits, RateLimitException, sleep_and_retry
-# consts for rate limiting feature
-MAX_CALLS_PER_MINUTE = 20
-ONE_MINUTE = 60
-class OpenAIClient:
-    '''
-    Leverage the trained GPT-3 Model posted by OpenAI for multiple usages (mainly for data aug in our project)
-    '''
-    def __init__(self):
-        '''
-        Notice you need to define the OPENAI_API_KEY in your .env file before using the OpenAIClient
-        Prepare the necessary environment for open ai gpt 3 model
-        '''
-        load_dotenv()
-        openai.api_key = os.getenv("OPENAI_API_KEY")
-        self.parapharase_prefix = "Paraphrase the following sentence: "
-        self.parapharase_model = "text-davinci-002"
 
-    @sleep_and_retry
-    @limits(calls=MAX_CALLS_PER_MINUTE, period=ONE_MINUTE)
-    def getParaphrasedSentence(self, sentence):
-        '''
-        The API calls is configured with the rate limiter (60 requests / minute)
-        Args:
-            sentence: original sentence which we try to paraphrase
-        Return:
-            paraphrased_sentence
-        '''
-        response = openai.Completion.create(
-        model=self.parapharase_model,
-        prompt=f"{self.parapharase_prefix}{sentence}",
-        temperature=0.7,
-        max_tokens=256,
-        top_p=1,
-        frequency_penalty=0,
-        presence_penalty=0
-        )
-        return self.getSentenceFromOpenAIResponse(response)
-    
-    
-    def getSentenceFromOpenAIResponse(self, response):
-        response_json = to_json_schema(response)
-        return response_json.get("choices")[0].get("text").strip()
+
+
+#
+# class OpenAIClient:
+#     '''
+#     Leverage the trained GPT-3 Model posted by OpenAI for multiple usages (mainly for data aug in our project)
+#     '''
+#     def __init__(self):
+#         '''
+#         Notice you need to define the OPENAI_API_KEY in your .env file before using the OpenAIClient
+#         Prepare the necessary environment for open ai gpt 3 model
+#         '''
+#         load_dotenv()
+#         openai.api_key = os.getenv("OPENAI_API_KEY")
+#         self.parapharase_prefix = "Paraphrase the following sentence: "
+#         self.parapharase_model = "text-davinci-002"
+#
+#     @sleep_and_retry
+#     @limits(calls=MAX_CALLS_PER_MINUTE, period=ONE_MINUTE)
+#     def getParaphrasedSentence(self, sentence):
+#         '''
+#         The API calls is configured with the rate limiter (60 requests / minute)
+#         Args:
+#             sentence: original sentence which we try to paraphrase
+#         Return:
+#             paraphrased_sentence
+#         '''
+#         response = openai.Completion.create(
+#         model=self.parapharase_model,
+#         prompt=f"{self.parapharase_prefix}{sentence}",
+#         temperature=0.7,
+#         max_tokens=256,
+#         top_p=1,
+#         frequency_penalty=0,
+#         presence_penalty=0
+#         )
+#         return self.getSentenceFromOpenAIResponse(response)
+#
+#
+#     def getSentenceFromOpenAIResponse(self, response):
+#         response_json = to_json_schema(response)
+#         return response_json.get("choices")[0].get("text").strip()
