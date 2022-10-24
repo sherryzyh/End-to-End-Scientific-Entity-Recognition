b2c12442482509f55a1389d9736ca8b3d6271f99
sherryzyh <sherryyh.z@gmail.com>
2022-10-23 20:51:08 -0400

    update env setup

--- setup.sh
+++ setup.sh
@@ -3,14 +3,14 @@ cudaversion=${2:-"11.6"}
 
 conda install -c conda-forge pypdf2 -y
 conda install typing_extensions -y
-conda install -c huggingface transformers
-conda install -c huggingface -c conda-forge datasets
+conda install -c huggingface transformers -y
+conda install -c huggingface -c conda-forge datasets -y
 if [${device} = "cpu"]; then
 #        echo ${device}
-        conda install pytorch torchvision torchaudio -c pytorch
+        conda install pytorch torchvision torchaudio -c pytorch -y
 else
 #        echo ${device} ${cudaversion}
-        conda install pytorch torchvision torchaudio cudatoolkit=${cudaversion} -c pytorch -c conda-forge
+        conda install pytorch torchvision torchaudio cudatoolkit=${cudaversion} -c pytorch -c conda-forge -y
 fi
 python3 -m pip install -r requirement.txt
 python3 -m spacy download en_core_web_sm
