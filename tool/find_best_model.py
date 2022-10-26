f534e4c6e67e990670ee23b669062fc5053b787e
sherryzyh <sherryyh.z@gmail.com>
2022-10-26 17:01:59 -0400

    update find best model

--- tool/find_best_model.py
+++ tool/find_best_model.py
@@ -8,7 +8,10 @@ if __name__ == '__main__':
     parser.add_argument('--experiment', '-e',
                         type=str,
                         default=None,
-                        help='Configuration file to use')
+                        help='Experiment Name')
+    parser.add_argument('--find_only', '-f',
+                        action='store_true',
+                        help='No need to save the best models')
     args = parser.parse_args()
 
     result_path = os.path.join("results", args.experiment)
@@ -20,18 +23,20 @@ if __name__ == '__main__':
     log_history = trainer_state["log_history"]
     epoch = int(trainer_state["epoch"])
 
-    min_eval_loss = [10000, None]
-    max_f1_score = [0, None]
+    min_eval_loss = [10000, None, None]
+    max_f1_score = [0, None, None]
     for e in range(epoch):
         eval_log = log_history[e * 2 + 1]
         if eval_log["eval_loss"] < min_eval_loss[0]:
-            min_eval_loss = [eval_log["eval_loss"], eval_log["step"]]
+            min_eval_loss = [eval_log["eval_loss"], eval_log["step"], e + 1]
         
         if eval_log["eval_f1"] > max_f1_score[0]:
-            max_f1_score = [eval_log["eval_f1"], eval_log["step"]]
+            max_f1_score = [eval_log["eval_f1"], eval_log["step"], e + 1]
     
-    shutil.copytree(os.path.join(result_path, f"checkpoint-{min_eval_loss[1]}"), os.path.join(result_path, "best_loss_model"))
-    shutil.copytree(os.path.join(result_path, f"checkpoint-{max_f1_score[1]}"), os.path.join(result_path, "best_f1_model"))
-
-    print(f"Best f1 score: {max_f1_score[0]}, at step {max_f1_score[1]}")
-    print(f"Min eval loss: {min_eval_loss[0]}, at step {min_eval_loss[1]}")
+    if not args.find_only:
+        print("Save best models")
+        shutil.copytree(os.path.join(result_path, f"checkpoint-{min_eval_loss[1]}"), os.path.join(result_path, "best_loss_model"))
+        shutil.copytree(os.path.join(result_path, f"checkpoint-{max_f1_score[1]}"), os.path.join(result_path, "best_f1_model"))
+    
+    print(f"Best f1 score: {max_f1_score[0]}, at step {max_f1_score[1]}, epoch {max_f1_score[2]}")
+    print(f"Min eval loss: {min_eval_loss[0]}, at step {min_eval_loss[1]}, epoch {min_eval_loss[2]}")
