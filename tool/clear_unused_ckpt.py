f655581cb61f87c0d56cc3b7253cfbfbde59bd97
sherryzyh <sherryyh.z@gmail.com>
2022-10-26 13:54:44 -0400

    clear checkpoints

--- tool/clear_unused_ckpt.py
+++ tool/clear_unused_ckpt.py
@@ -1,12 +1,35 @@
 import os
+import shutil
 from argparse import ArgumentParser
 
 if __name__ == '__main__':
     parser = ArgumentParser()
-    parser.add_argument('--result', '-r',
+    parser.add_argument('--experiment', '-e',
                         type=str,
                         default=None,
                         help='Configuration file to use')
     args = parser.parse_args()
 
-    
+    source = os.path.join("results", args.experiment)
+    dest_root = "results/confirmed/"
+    dest = os.path.join(dest_root, args.experiment)
+
+    if not os.path.exists(dest):
+        os.mkdir(dest)
+
+    for file in os.listdir(source):
+        if not os.path.isdir(os.path.join(source, file)):
+            shutil.copy2(os.path.join(source, file), dest)
+            continue
+
+        if file == "best_f1_model":
+            if not os.path.exists(os.path.join(dest, file)):
+                shutil.copytree(os.path.join(source, file), os.path.join(dest, file))
+            continue
+            
+        if file == "best_loss_model":
+            if not os.path.exists(os.path.join(dest, file)):
+                shutil.copytree(os.path.join(source, file), os.path.join(dest, file))
+            continue
+
+    shutil. rmtree(source)
