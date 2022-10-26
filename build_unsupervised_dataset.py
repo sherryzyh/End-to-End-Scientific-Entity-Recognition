77bc0ff25c06fbbfd9cc85848765e9102588f5df
sherryzyh <sherryyh.z@gmail.com>
2022-10-26 00:35:56 -0400

    update pretraining config files

--- build_unsupervised_dataset.py
+++ build_unsupervised_dataset.py
@@ -5,14 +5,14 @@ from collections import defaultdict
 if __name__ == '__main__':
     dataroot = "/work/yinghuan/data/SER_Unsupervised_Data"
 
-    destination = "/work/yinghuan/projects/End-to-End-Scientific-Entity-Recognition/Unsupervised_Subset"
+    destination = "/work/yinghuan/data/SER_Unsupervised_Data_Large"
 
     venue = defaultdict(lambda: 0)
     for file in os.listdir(os.path.join(dataroot, "tokenized_paper")):
         article_info = file.split("_")
         year = article_info[1]
         conference = article_info[2]
-        if venue[year+conference] > 5:
+        if venue[year+conference] > 30:
             continue
         tokenized_file = os.path.join(dataroot, "tokenized_paper", file)
         annotation_file = os.path.join(dataroot, "annotation_paper", "anno"+file[9:])
